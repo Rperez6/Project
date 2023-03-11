535006a462108fac56bc0202f7590038e9e08545
Rperez6 <98997388+Rperez6@users.noreply.github.com>
2023-03-10 19:21:21 -0600

    Before-testing

--- CRUD.py
+++ CRUD.py
@@ -158,92 +158,125 @@ def update_spaceship(ship_tag):
     execute_query(conn, update_query)
     return 'Spaceship tag ' +(ship_tag)+ ' updated successfully'
 
-# #API for cargo #########
+################## #API for cargo #########
 
-# #GET  cargo using ID http://127.0.0.1:5000/api/cargo?id=
-# @app.route('/api/cargo', methods=['GET']) # #
-# def api_cargo_id():
-#     if 'id' in request.args: 
-#         id = int(request.args['id'])
-#     else:
-#         return 'ERROR: No valid ID provided!'
+#GET  specific cargo using order ID http://127.0.0.1:5000/api/cargo/order?id=
+@app.route('/api/cargo/order', methods=['GET']) # #
+def api_cargo_id():
+    if 'id' in request.args: 
+        id = int(request.args['id'])
+    else:
+        return 'ERROR: No valid ID provided!'
     
-#     sql = "SELECT * FROM cargo WHERE id = %s" %(id)
-#     cargo = execute_read_query(conn, sql)
-#     if cargo:
-#         return jsonify(cargo[0])
-    
+    sql = "SELECT order_id,weight,cargotype,departure,arrival, ship_tag FROM cargo where order_id=%s" %id
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    records=cursor.fetchall()
+    cargos = []
+    for record in records:
+        cargo = {
+            'Order ID:  ': record[0],
+            'Max Weight of Ship: ': record[1],
+            'Cargo Type: ':record[2],
+            'Departure Date': record[3],
+            'Arrival Date': record[4],
+            'Ship Tag': record[5]
+        }
+        cargos.append(cargo)
+    response = {'cargo': cargos}
+    return jsonify(response)
 
-# #PUT new cargo using JSON format http://127.0.0.1:5000/api/cargo/(cargo ID) date format YYYY-MM-DD 
-# @app.route('/api/cargo/<int:id>', methods=['PUT'])
-# def update_cargo(id):
-#     request_data = request.json
-#     updname = request_data['name']
-#     updweight = request_data['weight']
-#     updcargotype = request_data['cargotype']
-#     updshipid = request_data['shipid']
-#     update_query = "UPDATE cargo SET name='%s', weight=%s, cargotype='%s',shipid=%s WHERE id=%s" % (updname,updweight,updcargotype,updshipid,id)
-#     execute_query(conn, update_query)
-#     return 'Cargo ID ' +str(id)+ ' updated successfully'
+#Get all cargo using http://127.0.0.1:5000/api/cargo/all
+@app.route('/api/cargo/all',methods=['GET'])
+def api_cargo_all():
+    sql = "SELECT order_id,weight, cargotype, departure, arrival, ship_tag FROM cargo"
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cargo_list = []
+    for row in result:
+        cargo = {
+           
+            'weight': row[1],
+            'cargotype': row[2],
+            'departure': str(row[3]) if row[3] else '',
+            'arrival': str(row[4]) if row[4] else '',
+            'ship_tag': row[5],
+            'order_id':row[0]
+        }
+        cargo_list.append(cargo)
+    return jsonify(cargo_list)
+
+#PUT (update) order_id/cargotype cargo using JSON format http://127.0.0.1:5000/api/cargo/update/(cargo ID) date format YYYY-MM-DD 
+@app.route('/api/cargo/update/<int:order_id>', methods=['PUT'])
+def update_cargo(order_id):
+    request_data = request.json
+    updorder = request_data['order_id']
+    updcargotype = request_data['cargotype']
+    update_query = "UPDATE cargo SET order_id=%s,cargotype='%s' WHERE order_id=%s" % (updorder,updcargotype,order_id)
+    execute_query(conn, update_query)
+    return 'Cargo ID ' +str(order_id)+ ' updated successfully'
 
 
-# #PUT cargo arrival time using JSON format http://127.0.0.1:5000/api/cargo/arrival/(cargo ID) UPDATING ARRIVAL TIME
-# @app.route('/api/cargo/arrival/order/<int:id>', methods=['PUT'])
-# def update_cargo_arrival(id):
-#     request_data = request.json 
-#     updarrival = request_data['arrival']
-#     checktime = "Select departure fROM cargo WHERE id=%s" %(id) #Retrieve cargo's departure time
-#     cursor = conn.cursor()
-#     cursor.execute(checktime)
-#     records = cursor.fetchall()
-#     if records:
-#         departure_str = records[0][0].strftime('%Y-%m-%d') #https://www.programiz.com/python-programming/examples/string-to-datetime
-#         departure = datetime.strptime(departure_str, '%Y-%m-%d')
-#         arrival = datetime.strptime(updarrival, '%Y-%m-%d')
-#         if arrival >= departure:
-#             update_query = "UPDATE cargo SET arrival='%s' WHERE id=%s" % (updarrival, id)
-#             execute_query(conn, update_query)
-#             return 'Cargo ID ' + str(id) + ' has been given an updated arrival time of ' + str(updarrival)
-#         else:
-#             return 'Arrival date cannot be before departure'
-#     else:
-#         return 'Cargo ID ' + str(id) + ' not found'    
+#PUT (update) cargo arrival time using JSON format http://127.0.0.1:5000/api/cargo/arrival/order/(order ID) UPDATING ARRIVAL TIME YYYY-MM-DD
+@app.route('/api/cargo/arrival/order/<int:order_id>', methods=['PUT'])
+def update_cargo_arrival(order_id):
+    request_data = request.json 
+    updarrival = request_data['arrival']
+    checktime = "Select departure fROM cargo WHERE order_id=%s" %(order_id) #Retrieve cargo's departure time
+    cursor = conn.cursor()
+    cursor.execute(checktime)
+    records = cursor.fetchall()
+    if records:
+        #compares time
+        departure_str = records[0][0].strftime('%Y-%m-%d') #https://www.programiz.com/python-programming/examples/string-to-datetime
+        departure = datetime.strptime(departure_str, '%Y-%m-%d')
+        arrival = datetime.strptime(updarrival, '%Y-%m-%d')
+        if arrival >= departure:
+            update_query = "UPDATE cargo SET arrival='%s' WHERE order_id=%s" % (updarrival, order_id)
+            execute_query(conn, update_query)
+            return 'Cargo ID ' + str(order_id) + ' has been given an updated arrival time of ' + str(updarrival)
+        else:
+            return 'Arrival date cannot be before departure'
+    else:
+        return 'Cargo ID ' + str(id) + ' not found'    
 
-# #Put cargo departure time using JSON format http://127.0.0.1:5000/api/cargo/departure/(cargo ID) ADDING DEPARTURE TIME
-# @app.route('/api/cargo/departure/order/<int:id>', methods=['PUT'])
-# def update_cargo_departure(id):
-#     request_data = request.json
-#     upddeparture = request_data['departure']
-#     insert = "UPDATE cargo SET departure='%s' WHERE =%s" %(upddeparture,id)
-#     execute_query(conn,insert)
-#     return 'Cargo ID ' +str(id) + ' has been updated with departure' +str(upddeparture)
+#Put (update) cargo departure time using JSON format http://127.0.0.1:5000/api/cargo/departure/(cargo ID) ADDING DEPARTURE TIME YYYY-MM-DD
+@app.route('/api/cargo/departure/<int:order_id>', methods=['PUT'])
+def update_cargo_departure(order_id):
+    request_data = request.json
+    upddeparture = request_data['departure']
+    insert = "UPDATE cargo SET departure='%s' WHERE order_id =%s" %(upddeparture,order_id)
+    execute_query(conn,insert)
+    return 'Cargo Order ' +str(order_id) + ' has been updated with departure ' +str(upddeparture)
 
 
-# #POST new cargo in JSON format in http://127.0.0.1:5000/api/cargo
-# @app.route('/api/cargo', methods=['POST'])
-# def api_cargo():
-#     request_data = request.get_json()
-#     newname = request_data['name']
-#     newweight = request_data['weight']
-#     newcargotype = request_data['cargotype']
-#     newshipname = request_data['shipname']
-#     checkweight = "SELECT maxweight FROM spaceship WHERE id=%s" %(newweight)
-#     cursor = conn.cursor()
-#     cursor.execute(checkweight)
-#     record = cursor.fetchone()
-#     if record and record[0] >= newweight:    
-#         add = "INSERT INTO cargo (name, weight, cargotype, shipname ) values ('%s', %s, '%s',%s)" %  (newname, newweight, newcargotype, newshipname)
-#         execute_query(conn, add)
-#         return("Cargo has been added successfully")    
-#     else:
-#         return("Ship is unable to support weight")
-    
-# #Delete cargo using ID in http://127.0.0.1:5000/api/cargo/(cargo ID)
-# @app.route('/api/cargo/<int:id>', methods= ['DELETE'])
-# def delete_cargp(id):
-#     delete = "DELETE FROM cargo WHERE id = %s" % (id)
-#     execute_query(conn, delete)
-#     return 'Cargo with ID ' +str(id)+' has been deleted.'
+#POST (add)new cargo in JSON format in http://127.0.0.1:5000/api/cargo
+@app.route('/api/cargo', methods=['POST'])
+def api_cargo():
+    request_data = request.get_json()
+    newasset = request_data['order_id']
+    newweight = request_data['weight']
+    newcargotype = request_data['cargotype']
+    newshiptag = request_data['ship_tag']
+    checkweight = "SELECT  maxweight, id FROM spaceship WHERE ship_tag= '%s'" %(newshiptag)
+    cursor = conn.cursor()
+    cursor.execute(checkweight)
+    record = cursor.fetchone()
+
+    if record and record[0] >= newweight:
+        add = "INSERT INTO cargo (order_id, weight, cargotype, ship_tag ,ship_id) values (%s, %s, '%s','%s',%s)" %  (newasset, newweight, newcargotype, newshiptag,record[1])
+        execute_query(conn, add)
+        return("Cargo has been added successfully")    
+    else:
+        return("Ship is unable to support weight")
+        
+#Delete cargo using ID in http://127.0.0.1:5000/api/cargo/delete/(order ID)
+@app.route('/api/cargo/delete/<int:order_id>', methods= ['DELETE'])
+def delete_cargp(order_id):
+    delete = "DELETE FROM cargo WHERE order_id = %s" % (order_id)
+    execute_query(conn, delete)
+    return 'Cargo with ID ' +str(order_id)+' has been deleted.'
 
 
 
